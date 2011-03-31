eb85e6b0e83487c1f075a962b61af1d4c942924b
Brendan Loudermilk <brendan@apwit.com>
2011-03-31 11:56:51 -0700

    Added component column to tickets index

--- assassembla.user.js
+++ assassembla.user.js
@@ -61,10 +61,30 @@
 $(function() {
 
   // Adds the "My Followed Tickets" link to the page header	
-	$("#user-box").find('span:first').before('<span>|</span><a href="/followed_tickets"><strong>My Followed Tickets</strong></a>');
+	$('#user-box').find('span:first').before('<span>|</span><a href="/followed_tickets"><strong>My Followed Tickets</strong></a>');
 
   // Removes the sidebar in the Tickets index
-  $("#tickets-right-col-wrap > tbody > tr > td:not(:last)").remove();
+  $('#tickets-right-col-wrap > tbody > tr > td:not(:last)').remove();
+
+  // Turns square brackets in ticket titles into a "Component" column
+  // on the tickets index
+  $('#ticket_list > table > thead > tr > th:first-child').after('<th class="component_column">Component</th>');
+
+  var components = [];
+  $('.component > li:not(:first)').each(function () {
+    components[$(this).attr('class').split('_')[1]] = $(this).children().text();
+  });
+
+  $('.ticket_row').each(function () {
+    var $this = $(this);
+    $this.next().text().replace(/new ContextMenu\((.*)\)/, function (m, cap) {
+      var component = components[$.parseJSON(cap).component_id] || '';
+      $this.children().first().after('<td class="number custom" style="color: black">' + component + '</td>');
+
+      // Replace expects a string to be returned
+      return "";
+    });
+  });
 
 });
 
